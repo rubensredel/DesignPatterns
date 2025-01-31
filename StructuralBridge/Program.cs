4deeeb8bb417200b0d5218fb6b1c432d3fc760ab
Rubens Redel <rubensredel@gmail.com>
2025-01-31 08:03:47 -0300

    feat: bridge pattern

---
+++ StructuralBridge/Program.cs
@@ -0,0 +1,114 @@
+﻿using StructuralBridge;
+
+var mustang = new Mustang(new EngineV8());
+var maverick = new Maverick(new EngineV6());
+var tracker = new Tracker(new EngineV4());
+
+mustang.Start();
+mustang.Drive();
+mustang.Stop();
+maverick.Start();
+maverick.Drive();
+maverick.Stop();
+tracker.Start();
+tracker.Drive();
+tracker.Stop();
+
+namespace StructuralBridge
+{
+    abstract class Vehicle(IEngine engine)
+    {
+        protected IEngine engine = engine;
+
+        public virtual void Drive()
+        {
+            engine.IncreasePower();
+            engine.DecreasePower();
+        }
+        public abstract void Start();
+        public abstract void Stop();
+    }
+
+    interface IEngine
+    {
+        void Start();
+        void Stop();
+        void IncreasePower();
+        void DecreasePower();
+    }
+
+    class EngineV8 : IEngine
+    {
+        public void Start() => Console.WriteLine("Starting V8 engine");  
+        public void Stop() => Console.WriteLine("Stopping V8 engine");
+        public void IncreasePower() => Console.WriteLine("Increasing power of V8 engine");
+        public void DecreasePower() => Console.WriteLine("Decreasing power of V8 engine");
+    }
+
+    class EngineV6 : IEngine
+    {
+        public void Start() => Console.WriteLine("Starting V6 engine");
+        public void Stop() => Console.WriteLine("Stopping V6 engine");
+        public void IncreasePower() => Console.WriteLine("Increasing power of V6 engine");
+        public void DecreasePower() => Console.WriteLine("Decreasing power of V6 engine");
+    }
+
+    class EngineV4 : IEngine
+    {
+        public void Start() => Console.WriteLine("Starting V4 engine");
+        public void Stop() => Console.WriteLine("Stopping V4 engine");
+        public void IncreasePower() => Console.WriteLine("Increasing power of V4 engine");
+        public void DecreasePower() => Console.WriteLine("Decreasing power of V4 engine");
+    }
+
+    class Mustang(IEngine engine) : Vehicle(engine)
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Mustang is driving");
+            base.Drive();
+        }
+        public override void Start()
+        {
+            engine.Start();
+        }
+        public override void Stop()
+        {
+            engine.Stop();
+        }
+    }
+
+    class Maverick(IEngine engine) : Vehicle(engine)
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Maverick is driving");
+            base.Drive();
+        }
+        public override void Start()
+        {
+            engine.Start();
+        }
+        public override void Stop()
+        {
+            engine.Stop();
+        }
+    }
+
+    class Tracker(IEngine engine) : Vehicle(engine)
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Tracker is driving");
+            base.Drive();
+        }
+        public override void Start()
+        {
+            engine.Start();
+        }
+        public override void Stop()
+        {
+            engine.Stop();
+        }
+    }
+}
